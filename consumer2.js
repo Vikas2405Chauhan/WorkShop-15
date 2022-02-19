f0cb2c43b50923a71d256bb321dd7c24ac76bdf5
Vikas2405Chauhan <vikas@leewayhertz.com>
2022-02-19 13:53:36 +0530

    add all file

---
+++ consumer2.js
@@ -0,0 +1,20 @@
+const amqp = require("amqplib"); //advance massage queue protocol library
+
+connect();
+async function connect() {
+    try {
+        const connection = await amqp.connect("amqp://localhost:5672");
+        const channel = await connection.createChannel();
+        const result = await channel.assertQueue("jobs");
+
+        channel.consume("jobs", message => {
+            const input = JSON.parse(message.content.toString());
+            console.log(`Consumer 2 Recieved job with input ${input.number}`);
+        })
+
+        console.log("Waiting for messages..!!");
+
+    } catch (ex) {
+        console.error(ex);
+    }
+}
